04097c388596828f8e6fff1d7090de9863390d53
wdb <wdb@mz.co.kr>
2023-07-30 03:46:35 +0900

    refactor : arrange code format

--- week_04/02_modulize/variable.tf
+++ week_04/02_modulize/variable.tf
@@ -6,12 +6,6 @@ variable "region" {}
 variable "prefix" {}
 
 # sg
-variable "sg_def" {
-  type = map(any)
-}
-variable "sg_ingress_web" {
-  type = map(any)
-}
-variable "sg_ingress_db" {
-  type = map(any)
-}
+variable "sg_def" {}
+variable "sg_ingress_web" {}
+variable "sg_ingress_db" {}
